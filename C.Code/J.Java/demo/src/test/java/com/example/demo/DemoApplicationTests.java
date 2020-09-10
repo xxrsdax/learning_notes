9654548f96154c51db9768905fb9f2746e7d10aa
ax <455916695@qq.com>
2020-09-10 08:13:29 +0800

    demo

---
+++ C.Code/J.Java/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() throws Exception {
+
+
+
+    }
+
+}
